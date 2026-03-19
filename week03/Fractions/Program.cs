e7aed6355a8748804f20beffb7c8a18ab1630776
mfumattew <fmabouba@byupathway.edu>
2026-03-19 19:11:52 +0100

    Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.ComponentModel.Design.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
-    }
+        Fraction w = new Fraction ();
+        Console.WriteLine(w.GetFractionString());
+        Console.WriteLine(w.GetDecimmalValue());
+
+        Fraction x = new Fraction(5);
+        Console.WriteLine(x.GetFractionString());
+        Console.WriteLine(x.GetDecimmalValue());
+
+        Fraction y = new Fraction (3, 4);
+        Console.WriteLine(y.GetFractionString());
+        Console.WriteLine(y.GetDecimmalValue());
+
+        Fraction z = new Fraction (1, 3);
+        Console.WriteLine(z.GetFractionString());
+        Console.WriteLine(z.GetDecimmalValue());
+    }   
 }
