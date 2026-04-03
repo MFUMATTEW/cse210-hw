80f093766c79cebad495d7f4b541701136d08d87
mfumattew <fmabouba@byupathway.edu>
2026-04-03 20:44:30 +0100

    Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment_x = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assignment_x.GetSummary());
+
+        MathAssigment math_x = new MathAssigment("Samuel Blessing", "Multiplication","7.3","8-19" );
+        Console.WriteLine(math_x.GetSummary());
+        Console.WriteLine(math_x.GetHomeworkList());
+
+        WritingAssignment writing_x = new WritingAssignment("Flavien Mabouba", "African History", "Bantu Empire");
+        Console.WriteLine(writing_x.GetSummary());
+        Console.WriteLine(writing_x.GetWritingInformation());
     }
 }
