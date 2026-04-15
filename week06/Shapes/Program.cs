a724e631f65c444686770b50a92be1313000eaa3
mfumattew <fmabouba@byupathway.edu>
2026-04-15 17:49:49 +0100

    Polymorphism Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square carré = new Square("blue",2);
+        Console.WriteLine($"The {carré.GetColor()} square's area is {carré.GetArea()}cm^2");
+
+        Rectangle rectangle = new Rectangle("red", 2, 4);
+        Console.WriteLine($"The {rectangle.GetColor()} rectangle's area is {rectangle.GetArea()}cm^2");
+
+        Circle circle = new Circle("green", 5);
+        Console.WriteLine($"The {circle.GetColor()} circle's area is {circle.GetArea()}cm^2");
+        
+        
     }
 }
